6e2d7cd80c66f3739ca8bdd7e569474cc57af3e2
Asel Petrova <asel.petrova@gmail.com>
2016-07-05 23:24:07 +0300

    more then 3 letter more than 1 word

--- src/main/java/ReadExcelDemo.java
+++ src/main/java/ReadExcelDemo.java
@@ -11,12 +11,9 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReadExcelDemo
-{
-    public static void main(String[] args)
-    {
-        try
-        {
+public class ReadExcelDemo {
+    public static void main(String[] args) {
+        try {
             FileInputStream file = new FileInputStream(new File("B777ATA25.xlsx"));
             FileInputStream fileata = new FileInputStream(new File("subata.xlsx"));
 
@@ -29,11 +26,11 @@ public class ReadExcelDemo
             XSSFSheet sheet = workbook.getSheetAt(1);
             XSSFSheet sheetata = wbsubata.getSheetAt(0);
 
-            deleteColumn(sheet,25);
+            deleteColumn(sheet, 25);
 
 
             //Iterate through each rows one by on
-            for (int i = 1; i < sheet.getLastRowNum() ; ++i) {
+            for (int i = 1; i < sheet.getLastRowNum(); ++i) {
 
                 Row row = sheet.getRow(i);
                 //For each row, iterate through all the columns
@@ -42,38 +39,47 @@ public class ReadExcelDemo
                 Cell colY = row.getCell(24);
                 Cell colL = row.getCell(11);
 
-
+                //System.out.println(colL);
 
                 if (colE == null || colY == null || colL == null) continue;
                 if (colL.getStringCellValue().equals("N")) continue;
 
                 String cellE = colE.getStringCellValue();
-                System.out.println(cellE);
+                System.out.println("Origin subata:" + cellE);
 
                 String cellYsh = subata(colY);
                 if ("not found".equals(cellYsh)) {
-                    for ( int k = 1; k < sheetata.getLastRowNum(); ++k) {
+                    for (int k = 1; k < sheetata.getLastRowNum(); ++k) {
                         Row rowata = sheetata.getRow(k);
 
                         Cell colA = rowata.getCell(0);
                         Cell colB = rowata.getCell(1);
+                        String[] colBshort = validateCellata(colB).split(" ");
+                        int qtyWord = 0;
 
-                        if (StringUtils.containsAny(colY.getStringCellValue().toUpperCase(), validateCellata(colB).split(" "))) {
-                            Pattern patternata = Pattern.compile("(\\d{2}-+\\d{2}-+\\d{2})");
-                            Matcher matcherata = patternata.matcher(colA.getStringCellValue());
-                            if (matcherata.find()) {
-                                cellYsh = matcherata.group(0).substring(0,5).replace("-","");
-                                System.out.println(cellYsh + " // " + colY.getStringCellValue().toUpperCase() + " // " + Arrays.toString(validateCellata(colB).split(" "))) ;
-
+                        String allWords = "";
+                        for (String word : colBshort) {
+                            if (word.length() > 3 && colY.getStringCellValue().toUpperCase().contains(word) && !allWords.contains(word)) {
+                                qtyWord++;
+                                allWords += word + " ";
                             }
                         }
 
+
+                        if (qtyWord > 1) {
+                            Pattern patternata = Pattern.compile("(\\d{2}-+\\d{2}-+\\d{2})");
+                            Matcher matcherata = patternata.matcher(colA.getStringCellValue());
+                            if (matcherata.find()) {
+                                cellYsh = matcherata.group(0).substring(0, 5).replace("-", "");
+                                System.out.println("Subata:" + cellYsh + "\nTEMPLATE:" + colY.getStringCellValue().toUpperCase() + "\nFOUND:" + allWords);
+                            }
+                        }
                     }
                 }
 //                System.out.println(cellYsh);
 
-                if  (!cellE.trim().equals(cellYsh.trim())) {
-                    Cell cell_new = row.createCell(row.getLastCellNum() );
+                if (!cellE.trim().equals(cellYsh.trim())) {
+                    Cell cell_new = row.createCell(row.getLastCellNum());
                     cell_new.setCellValue(cellYsh);
                 }
             }
@@ -82,20 +88,19 @@ public class ReadExcelDemo
             FileOutputStream out = new FileOutputStream(new File("B777ATA25.xlsx"));
             workbook.write(out);
             out.close();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-        public static String subata(Cell celly){
-            Pattern pattern = Pattern.compile("AMM.*(\\d{2}-+\\d{2}-+\\d{2})");
-            Matcher matcher = pattern.matcher(validateCell(celly));
-            if (matcher.find()) {
-                return matcher.group(1).substring(0,5).replace("-","");
-            }
-        return "not found";
+
+    public static String subata(Cell celly) {
+        Pattern pattern = Pattern.compile("AMM.*(\\d{2}-+\\d{2}-+\\d{2})");
+        Matcher matcher = pattern.matcher(validateCell(celly));
+        if (matcher.find()) {
+            return matcher.group(1).substring(0, 5).replace("-", "");
         }
+        return "not found";
+    }
 
     private static String validateCell(Cell celly) {
         return celly.getStringCellValue()
@@ -109,20 +114,21 @@ public class ReadExcelDemo
                 .replaceAll("\n", "")
                 .replaceAll("-", "")
                 .replaceAll("  ", " ")
-                .replaceAll(" AND ", " ");
+                .replaceAll(" AND ", " ")
+                .replaceAll(" TEST ", " ");
     }
 
 
-    public static void deleteColumn (XSSFSheet sheettodel, int rowtodel){
-            for (int j=0; j <= sheettodel.getLastRowNum(); j++) {
-                Row row = sheettodel.getRow(j);
-                if  (row.getLastCellNum() >= rowtodel) {
-                    Cell celltodel = row.getCell(rowtodel);
-                    if (celltodel == null) continue;
-                    row.removeCell(celltodel);
-                }
-
+    public static void deleteColumn(XSSFSheet sheettodel, int rowtodel) {
+        for (int j = 0; j <= sheettodel.getLastRowNum(); j++) {
+            Row row = sheettodel.getRow(j);
+            if (row.getLastCellNum() >= rowtodel) {
+                Cell celltodel = row.getCell(rowtodel);
+                if (celltodel == null) continue;
+                row.removeCell(celltodel);
             }
+
+        }
     }
 
 
